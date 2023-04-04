2d025c227462de8b77c6c9da22a4d52e8fdf4053
Rahul Kumar <121325179+Rahulkumar132@users.noreply.github.com>
2023-04-04 14:51:29 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+# Code-Alpha-Course-provider-platform-
+let menu = document.querySelector('#menu-btn');
+let header = document.querySelector('.header');
+
+menu.onclick = function(){
+  menu.classList.toggle('fa-times');
+  header.classList.toggle('active');
+}
+
+window.onscroll = function(){
+  menu.classList.remove('fa-times');
+  header.classList.remove('active');
+}let menu = document.querySelector('#menu-btn');
+let header = document.querySelector('.header');
+
+menu.onclick = function(){
+  menu.classList.toggle('fa-times');
+  header.classList.toggle('active');
+}
+
+window.onscroll = function(){
+  menu.classList.remove('fa-times');
+  header.classList.remove('active');
+}
